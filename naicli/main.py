275007784861aec354690e238b47154893c935b5
dagal <dagal@vivaldi.net>
2021-07-14 14:54:13 -0400

    fix buffers "memory leak"
    Added an option to disable the buffers' undo and redo stacks, which story copies of the whole input text.
    Will have to use the modified files in naicli/overrides while waiting for my PR to get accepted.

--- naicli/main.py
+++ naicli/main.py
@@ -6,9 +6,14 @@ from .story import *
 def main(argv=sys.argv):
     book = open_story_file("example/grug.story")
     #repl.echo(assemble_story_fragments(book.content.story))
-    repl.print_pager(assemble_story_datablocks(book.content.story))
+    #repl.print_pager(assemble_story_datablocks(book.content.story))
     #assemble_story_datablocks(book.content.story)
-    repl.start_prompt()
+    repl.start_prompt(assemble_story_datablocks(book.content.story))
+    # bigtext = ""
+    # with open("../book.txt") as bigfile:
+        # bigtext = bigfile.read()
+        
+    repl.start_prompt(bigtext)
 
 if __name__ == "__main__":
     main(sys.argv)
--- naicli/repl.py
+++ naicli/repl.py
@@ -1,14 +1,24 @@
 import click
 
-from prompt_toolkit import prompt
+from prompt_toolkit import print_formatted_text as print
+from prompt_toolkit.history import DummyHistory
+from .overrides.prompt import prompt, PromptSession
+#from prompt_toolkit import prompt, PromptSession
 
 from . import constants
+
 
 # https://github.com/pallets/click/issues/1905
 print_pager = print if constants.WINDOWS else click.echo_via_pager
 
-def start_prompt():
+def start_prompt(initial_text: str = "") -> None:
+    s = PromptSession(multiline=True, history=DummyHistory(), enable_undo_redo=False)
+    text = initial_text
+
     while 1:
-        user_input = prompt('>')
+        text = s.prompt(default=text)
+        click.clear()
+        #gc.collect()
         #message = click.edit(user_input)
-        print(user_input)
+        #print(user_input)
+        #sys.stdout.write(user_input)
