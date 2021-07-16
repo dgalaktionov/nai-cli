05d1a92296d1f0bdcbc1d052dc69579f27dd6760
dagal <dagal@vivaldi.net>
2021-07-15 23:20:31 -0400

    util.exponential_search

--- naicli/util.py
+++ naicli/util.py
@@ -1,3 +1,5 @@
+from typing import TypeVar, Sequence, Callable, Optional
+
 def join_strings(*strings: str) -> str:
     # when there's only a few of them, it's faster with fstrings (tested on Python 3.9)
     if len(strings) == 2:
@@ -6,3 +8,36 @@ def join_strings(*strings: str) -> str:
         return f"{strings[0]}{strings[1]}{strings[2]}"
     else:
         return "".join(strings)
+
+TINDEX = TypeVar("TINDEX")
+TVAL = TypeVar("TVAL")
+def exponential_search(array: Sequence[TINDEX], value: TVAL, key: Optional[Callable[[TINDEX], TVAL]] = None, from_end = False):
+    """
+        Using exponential search over a sorted array, it returns the last (rightmost) position of a value using exponential search.
+        If there is no such value in the array, a corresponding insertion position is returned.
+        Analogous to bisect.bisect_right.
+        Like with the function sorted, it is possible to provide a mapping function from the type of the array to the type of the value.
+        When from_end is set, the search is performed starting from the end of the array (right-to-left).
+    """
+    if len(array) == 0: return 0
+    if not key: key = lambda x: x
+    
+    i = 1
+    start, end = 0, len(array)
+    
+    # exponential search phase
+    if from_end:
+        i = -1
+        while -i < len(array) and key(array[i]) > value: i *= 2
+        start, end = max(0, i+len(array)), len(array)-i//-2
+    else:
+        while i < len(array) and key(array[i]) <= value: i *= 2
+        start, end = i//2, min(i+1, len(array))
+   
+    #binary search phase
+    while start < end:
+        i = (start+end)//2
+        if value < key(array[i]): end = i
+        else: start = i+1
+    
+    return start
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,12 +1,34 @@
 import pytest
+import bisect
 
-from hypothesis import given
-from hypothesis.strategies import text, lists
+from hypothesis import given, assume
+from hypothesis.strategies import text, lists, integers, tuples
 
-from typing import List
+from typing import List, Tuple
 
 from naicli.util import *
 
 @given(lists(text()))
 def test_join_strings(strings: List[str]):
     assert "".join(strings) == join_strings(*strings)
+
+def _i100():
+    return integers(min_value=-100, max_value=100)
+
+@given(lists(_i100()).map(sorted), _i100())
+def test_exponential_search(array: List[int], value: int):
+    assume(len(array) > 0)
+    assert exponential_search(array, value) == exponential_search(array, value, from_end=True) == bisect.bisect_right(array, value)
+    
+    for _ in range(4):
+        bisect.insort(array, value)
+        assert exponential_search(array, value) == exponential_search(array, value, from_end=True) == bisect.bisect_right(array, value)
+
+@given(lists(tuples(_i100(), _i100())), _i100())
+def test_exponential_key_search(array: List[Tuple[int,int]], value: int):
+    assume(len(array) > 0)
+    key = lambda a: a[1]
+    array = sorted(array, key=key)
+    mapped_array = list(map(key, array))
+    assert exponential_search(array, value, key=key) == exponential_search(array, value, key=key, from_end=True) \
+        == bisect.bisect_right(mapped_array, value)
