331d95e3960def9f561da874021d363724889371
dagal <dagal@vivaldi.net>
2021-07-19 21:15:27 -0400

    Sanitize dependencies.
    Added windows-curses as a conditional dependency and also removed all references to prompt_toolkit.

--- naicli/repl.py
+++ naicli/repl.py
@@ -1,20 +1,7 @@
 import click
 
-from prompt_toolkit import print_formatted_text as print
-from prompt_toolkit.history import DummyHistory
-from .overrides.prompt import prompt, PromptSession
-#from prompt_toolkit import prompt, PromptSession
-
 from . import constants
 
 
-# https://github.com/pallets/click/issues/1905
-print_pager = print if constants.WINDOWS else click.echo_via_pager
-
 def start_prompt(initial_text: str = "") -> None:
-    s = PromptSession(multiline=True, history=DummyHistory(), enable_undo_redo=False)
-    text = initial_text
-
-    while 1:
-        text += s.prompt(text)
-        click.clear()
+    print("TODO")
--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 if sys.version_info < (3,6):
     sys.exit("This program requires python 3.6 or above.")
 
+REQUIREMENTS=['click>=8.0.0']
+if sys.platform.startswith("win"): REQUIREMENTS.append('windows-curses>=2.0.0')
+
 setup(
     name='nai-cli',
     version='0.1.0',
@@ -13,7 +16,7 @@ setup(
     author='dagal',
     url='',
     packages=find_packages(include=['naicli']),
-    install_requires=['click>=8.0.0', 'prompt-toolkit>=3.0.0', 'pygments>=2.0.0'],
+    install_requires=REQUIREMENTS,
     entry_points={'console_scripts': ['nai-cli = naicli.main:main']},
     tests_require=['pytest', 'hypothesis']
 )
